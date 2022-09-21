8ee90fe963fce51c284bc4eb3863d8ca97125333
choiyoungjin <cyjin6789@gmail.com>
2022-09-21 09:44:54 +0900

    [mod] clothesMap data type 변경

--- datastructure/패션왕 신해빈.kt
+++ datastructure/패션왕 신해빈.kt
@@ -2,29 +2,29 @@
 
 fun main() {
     val br = System.`in`.bufferedReader()
-    lateinit var clothesMap: HashMap<String, MutableList<String>>
+    lateinit var clothesMap: HashMap<String, Int>
 
     repeat(br.readLine().toInt()) { // 테스트 케이스 입력
         clothesMap = hashMapOf()
 
         repeat(br.readLine().toInt()) { // 의상 입력
-            val (clothes, type) = br.readLine().split(" ")
+            val (_, type) = br.readLine().split(" ")
             if (clothesMap.containsKey(type))
-                clothesMap[type]!!.add(clothes)
+                clothesMap[type] = clothesMap[type]!!.plus(1)
             else
-                clothesMap[type] = mutableListOf(clothes)
+                clothesMap[type] = 1
         }
-        
+
         println(getNumOfClothesCombinations(clothesMap))
     }
 }
 
-fun getNumOfClothesCombinations(clothesMap: HashMap<String, MutableList<String>>): Int {
+fun getNumOfClothesCombinations(clothesMap: HashMap<String, Int>): Int {
     if (clothesMap.size == 0) return 0
 
     var count = 1
     clothesMap.forEach {
-        count *= it.value.size + 1 // +1은 해당 type의 의상은 입지 않는 경우를 추가한 것
+        count *= it.value + 1 // +1은 해당 type의 의상은 입지 않는 경우를 추가한 것
     }
 
     return count - 1 // -1은 어떠한 의상도 입지 않는 경우를 제외한 것
