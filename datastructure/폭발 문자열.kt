27e955a1024648344fedb796c8f2cfc6ce03aeaf
choiyoungjin <cyjin6789@gmail.com>
2022-09-24 05:52:21 +0900

    [mod] 폭발 문자열 발견 시 스택에서 해당 문자열을 제거하도록 수정

--- datastructure/폭발 문자열.kt
+++ datastructure/폭발 문자열.kt
@@ -1,22 +1,19 @@
 // https://www.acmicpc.net/problem/9935
 
-
 fun main() {
     val br = System.`in`.bufferedReader()
     val str = br.readLine()
-    val bomb = br.readLine().reversed()
-    var stack = mutableListOf<Char>()
+    val bomb = br.readLine()
+    val stack = mutableListOf<Char>()
     val lastBombChar = bomb[bomb.length - 1]
 
-    str.reversed().forEach {c ->
+    str.forEach { c ->
         stack.add(c)
-        if (c == lastBombChar) {
-            if (stack.size - bomb.length < 0) return@forEach
-            if (stack.subList(stack.size - bomb.length, stack.size).joinToString("") == bomb) {
-                stack = stack.subList(0, stack.size - bomb.length)
-            }
+        if (stack.size - bomb.length < 0) return@forEach
+        if (c == lastBombChar && stack.subList(stack.size - bomb.length, stack.size).joinToString("") == bomb) {
+            repeat(bomb.length) { stack.removeLast() }
         }
     }
 
-    println(stack.reversed().joinToString("").ifEmpty { "FRULA" })
+    println(stack.joinToString("").ifEmpty { "FRULA" })
 }
