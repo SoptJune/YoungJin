a8674531db6812bf0c9aa610cebb2010e46df19b
choiyoungjin <cyjin6789@gmail.com>
2023-01-26 11:38:15 +0900

    [chore] 변수명 수정

--- search/선물 교환.kt
+++ search/선물 교환.kt
@@ -5,11 +5,11 @@ fun main() {
     // 1. 각 학생별 idx값, 받은 선물 갯수, 선택한 학생 두명의 idx값 정보를 초기화
     val br = System.`in`.bufferedReader()
     students = Array(br.readLine().toInt()) { idx -> Student(idx) }
-    students.forEach { s ->
+    students.forEach { student ->
         val (one, two) = br.readLine().split(" ").map { it.toInt() - 1 }
         students[one].numOgGift++
         students[two].numOgGift++
-        s.studentPair = Pair(one, two)
+        student.studentPair = Pair(one, two)
     }
 
     // 2. 선물을 2개 미만으로 받은 학생을 표시
@@ -23,9 +23,9 @@ fun main() {
     var idx: Int
     while (failure.isNotEmpty()) {
         idx = failure.removeLast().idx // 선물을 2개 미만으로 받은 학생 a를 선택
-        students[idx].studentPair.let { ids -> // 해당 학생이 선택한 두명의 학생은 학생 a에게 선물을 받지 못함 -1
-            if (--students[ids.first].numOgGift == 1) failure.add(students[ids.first]) // 받은 선물의 갯수가 2 - 1 = 1이 된 경우, failure에 새롭게 추가
-            if (--students[ids.second].numOgGift == 1) failure.add(students[ids.second])
+        students[idx].studentPair.let { idxes -> // 해당 학생이 선택한 두명의 학생은 학생 a에게 선물을 받지 못함 -1
+            if (--students[idxes.first].numOgGift == 1) failure.add(students[idxes.first]) // 받은 선물의 갯수가 2 - 1 = 1이 된 경우, failure에 새롭게 추가
+            if (--students[idxes.second].numOgGift == 1) failure.add(students[idxes.second])
         }
     }
     
